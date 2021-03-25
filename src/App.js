b0e81472663c1b05bc012502d034c142d6becfb4
Thomas Strub <thomstrub@gmail.com>
2021-03-24 22:06:59 -0700

    finds error in src/components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
+import { Burger, Menu } from './components';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <>
         <GlobalStyles />
         <div>
+        <Burger />
           <h1>Hello. This is burger menu tutorial</h1>
           <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
           <small>Icon made by Freepik from www.flaticon.com</small>
