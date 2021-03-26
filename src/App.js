239b5c2648448fa2c89323f7afdcd4ab21f420e3
Thomas Strub <thomstrub@gmail.com>
2021-03-25 23:05:43 -0700

    adds useRef to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {ThemeProvider} from 'styled-components';
 import { GlobalStyles } from './global';
 import { theme } from './theme';
 import { Burger} from './components/Burger';
 import { Menu} from './components/Menu';
+import { useOnClickOutside } from './hooks';
 
 function App() {
 
   const [open, setOpen] = useState(false);
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
 
   return (
     <ThemeProvider theme={theme}>
@@ -19,7 +22,7 @@ function App() {
           <img src="https://image.flaticon.com/icons/svg/2016/2016012.svg" alt="burger icon" />
           <small>Icon made by Freepik from www.flaticon.com</small>
         </div>
-        <div >
+        <div ref={node}>
           <Burger open={open} setOpen={setOpen} />
           <Menu  open={open} setOpen={setOpen} />
         </div>
